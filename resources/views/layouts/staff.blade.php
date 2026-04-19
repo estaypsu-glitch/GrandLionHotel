2e445594e2ea0f4472aee9b1093af22f742e25e7
estaypsu-glitch <estaypsu@gmail.com>
2026-04-19 08:59:33 +0800

    Update files

--- resources/views/layouts/staff.blade.php
+++ resources/views/layouts/staff.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Staff') - {{ config('app.name', 'The Grand Lion Hotel') }}</title>
-    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('vendor/bootstrap-icons/font/bootstrap-icons.min.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="{{ asset('brand/lion_logo.png') }}">
     <link rel="apple-touch-icon" href="{{ asset('brand/lion_logo.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -507,7 +507,7 @@
         @yield('content')
     </main>
 
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         (() => {
             document.addEventListener('submit', (event) => {
